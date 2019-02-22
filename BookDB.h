361c1b84a1b0741eb81cb2c2427d828ba683179e
phuzums <47841026+phuzums@users.noreply.github.com>
2019-02-21 16:42:00 -0800

    Add files via upload

---
+++ BookDB.h
@@ -0,0 +1,91 @@
+#pragma once
+#include <string>
+
+#define MAX_BOOKS 2048
+
+
+/* REQ
+ ISBN This is the International Standard Book Number. It is a unique number assigned to each book by the publisher.
+ Title The title of the book.
+ Author The book’s author.
+ Publisher The company that publishes the book.
+ Date Added The date the book was added to the inventory.
+ Quantity-On-Hand The number of copies of the book in inventory.
+ Wholesale Cost The price paid by Serendipity for each copy of the book.
+ Retail Price The price Serendipity is charging for each copy of the book.
+ */
+struct Date
+{
+    int day, month, year;
+};
+
+struct Book
+{
+    int ISBN;
+    double wholesaleCost, retailCost;
+    std::string title, author, publisher;
+    Date addedOn;
+    int quantity = 1;
+};
+
+/* REQ  List by Quantity. A list of all books in the inventory sorted by quantity on hand. The books with the greatest quantity on hand will be listed first.
+        List by Cost. A list of all books in the inventory, sorted by wholesale cost. The
+ books with the greatest wholesale cost will be listed first.
+        List by Age. A list of all books in the inventory, sorted by purchase date. The
+ books that have been in the inventory longest will be listed first.
+ */
+enum SORT_METHOD
+{
+    QUANTITY, COST, AGE;
+};
+
+class BookDB
+{
+    // Update : We should do option 2, otherwise the code in main will be huge and less organized...
+    // Ideally we should just make a function outside of this class in the .cpp / .h so that the class
+    // doesn't hold a function that doesn't need to be carried with instances of the DB... but i don't think
+    // he'll care...
+public:                                        // We can do any of these
+    BookDB();                                    // 1. Create the database then initialize it later using some funciton like myBookDB.init("filename.txt");
+    BookDB(std::string filename);                // 2. Require the filename and parse the text file within the code
+    BookDB(Book* bookdatabase, int numBooks);    // 3. Parse the textfile elsewhere and just take an array of books and len of that array
+    ~BookDB();
+public:
+    // Getters
+            //!! REQ Inventory List. A list of information on all books in the inventory.
+    Book*    getBooks();
+    Book     getBook(int index);
+    int      getNumBooks();
+    
+            //!! REQ Inventory Wholesale Value A list of the wholesale value of all books in the inventory and the total wholesale value of the inventory.
+    double   getWholesaleValue();
+    
+            //!! REQ Inventory Retail Value. A list of the retail value of all books in the inventory and the total retail value of the inventory.
+    double  getRetailValue();
+    
+    // Mutators
+    void    readBooks(std::string filename);    // Needed if we go with option 1 or 2
+    void    writeBooks(std::string filename);
+            //!! REQ When a book is purchased, subtract it from the inventory file !!//
+    double    sellBook(int ISBN);                 // These cannot be combined...
+            //!! REQ Add, change, delete, and look up books in the inventory file !!//
+    bool    removeBook(Book bk);                // Sellbook does sales data, then calls rembk
+    void    removeBook(std::string bkName);
+    void    removeBook(int ISBN, int quantity);               // <--probably just use this one
+    void    addBook(Book bk);
+    int     findBook(ISBN);                     // Returns the index in our array of the found entry or -1 if not found
+    void    modifyBook(int ISBN, Book replacement);
+    int     findBook(std::string author);
+    int     findBook(something else?);
+    bool    sortBooks(SORT_METHOD sm);
+    
+private:
+    Book*   books;
+    int     numBooks;
+    double  wholesaleValue;
+    double  retailValue;
+    std::string DBFilename;
+    std::fstream DBFile;
+};
+
+
--- main.cpp
+++ main.cpp
@@ -1,1 +1,83 @@
-// meow
+#include "BookDB.h"
+
+//NOTES::
+//  - ISBN13's look like dis: 978-1-56931-507-1... i think we should be able to just strip the dashes, right?
+//  - ISBN10's look like 164273005X ... the chars would be more work search-wise... let's just use 13's w/o dashes?
+int main(int argc, char** argv)
+{
+    
+    int isbn;
+    // Test stuff out before we have file I/O working...
+    // so... make the structs by hand first.
+    Date today;
+    today.day   = 21;
+    today.month = 2;
+    today.year  = 2019;
+    
+    Book PokemonAdventures;
+    PokemonAdventures.ISBN          = 9781569315071;
+    PokemonAdventures.wholesaleCost = 474.00;
+    PokemonAdventures.retailCost    = 512.99;
+    PokemonAdventures.title         = "Pokemon Adventures";
+    PokemonAdventures.author        = "Hidenori Kusaka";
+    PokemonAdventures.publisher     = "Shogakukan";
+    PokemonAdventures.addedOn       = today;
+    
+    Book HinaMatsuri;
+    HinaMatsuri.ISBN                = 9781642730050;
+    HinaMatsuri.wholesaleCost       = 9.01;
+    HinaMatsuri.retailCost          = 9.53;
+    HinaMatsuri.title               = "Hinamatsuri Volume 1";
+    HinaMatsuri.author              = "Masao Ohtake";
+    HinaMatsuri.publisher           = "One Peace Books";
+    HinaMatsuri.addedOn             = today;
+    
+    Book LewdAnimeh;
+    LewdAnimeh.ISBN                 = 9784063840308;
+    LewdAnimeh.wholesaleCost        = 7.89;
+    LewdAnimeh.retailCost           = 99999999999.99; //#worthit
+    LewdAnimeh.title                = "Seitokai Yakuindomo 1";
+    LewdAnimeh.author               = "Tozen Ujiie";
+    LewdAnimeh.publisher            = "Kodansha";
+    LewdAnimeh.addedOn              = today;
+    
+    BookDB myDB("not_implemented_yet.nyan");
+    myDB.addBook(PokemonAdventures);
+    myDB.addBook(HinaMatsuri);
+    myDB.addBook(LewdAnimeh);
+    
+    // cout test stuff
+    
+    myDB.sortBy(SORT_METHOD::COST);
+    
+    // cout test stuff
+    
+    myDB.sellBook(9784063840308);
+    
+    // cout test stuff
+    
+    for(int idx = 0; idx < 2000; idx++)
+        myDB.addBook(HinaMatsuri);
+    
+    // more test stuff
+    
+    myDB.sortBy(SORT_METHOD::QUANTITY);
+    
+    // meow test stuff...
+    
+    
+    
+    
+    
+    
+    cout << "which book to remove?";
+    cin >> isbn;
+    
+    if (myDB.removeBook(isbn))
+        cout << "book was removed";
+    else
+        cout << "err. book not on hand...";
+    
+    return 0;
+    
+}
