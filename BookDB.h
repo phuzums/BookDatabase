8074f56bc29c0b8dd696ceba62de90d491e6562c
phuzums <47841026+phuzums@users.noreply.github.com>
2019-03-03 15:30:38 -0800

    Update BookDB.h
    see readme for changes

--- BookDB.h
+++ BookDB.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <fstream>
 
 #define MAX_BOOKS 2048
 
@@ -16,76 +17,66 @@
  */
 struct Date
 {
-    int day, month, year;
+	int day, month, year;
 };
 
-struct Book
+class Book
 {
-    int ISBN;
-    double wholesaleCost, retailCost;
-    std::string title, author, publisher;
-    Date addedOn;
-    int quantity = 1;
+public:
+	unsigned long ISBN;
+	double wholesaleCost, retailCost;
+	std::string title, author, publisher;
+	Date addedOn;
+	int quantity = 1;
+	friend std::ostream& operator<<(std::ostream &out, const Book &bk);
 };
 
-/* REQ  List by Quantity. A list of all books in the inventory sorted by quantity on hand. The books with the greatest quantity on hand will be listed first.
-        List by Cost. A list of all books in the inventory, sorted by wholesale cost. The
- books with the greatest wholesale cost will be listed first.
-        List by Age. A list of all books in the inventory, sorted by purchase date. The
- books that have been in the inventory longest will be listed first.
- */
 enum SORT_METHOD
 {
-    QUANTITY, COST, AGE;
+	QUANTITY = 0, COST, AGE
 };
 
 class BookDB
 {
-    // Update : We should do option 2, otherwise the code in main will be huge and less organized...
-    // Ideally we should just make a function outside of this class in the .cpp / .h so that the class
-    // doesn't hold a function that doesn't need to be carried with instances of the DB... but i don't think
-    // he'll care...
-public:                                        // We can do any of these
-    BookDB();                                    // 1. Create the database then initialize it later using some funciton like myBookDB.init("filename.txt");
-    BookDB(std::string filename);                // 2. Require the filename and parse the text file within the code
-    BookDB(Book* bookdatabase, int numBooks);    // 3. Parse the textfile elsewhere and just take an array of books and len of that array
-    ~BookDB();
 public:
-    // Getters
-            //!! REQ Inventory List. A list of information on all books in the inventory.
-    Book*    getBooks();
-    Book     getBook(int index);
-    int      getNumBooks();
-    
-            //!! REQ Inventory Wholesale Value A list of the wholesale value of all books in the inventory and the total wholesale value of the inventory.
-    double   getWholesaleValue();
-    
-            //!! REQ Inventory Retail Value. A list of the retail value of all books in the inventory and the total retail value of the inventory.
-    double  getRetailValue();
-    
-    // Mutators
-    void    readBooks(std::string filename);    // Needed if we go with option 1 or 2
-    void    writeBooks(std::string filename);
-            //!! REQ When a book is purchased, subtract it from the inventory file !!//
-    double    sellBook(int ISBN);                 // These cannot be combined...
-            //!! REQ Add, change, delete, and look up books in the inventory file !!//
-    bool    removeBook(Book bk);                // Sellbook does sales data, then calls rembk
-    void    removeBook(std::string bkName);
-    void    removeBook(int ISBN, int quantity);               // <--probably just use this one
-    void    addBook(Book bk);
-    int     findBook(ISBN);                     // Returns the index in our array of the found entry or -1 if not found
-    void    modifyBook(int ISBN, Book replacement);
-    int     findBook(std::string author);
-    int     findBook(something else?);
-    bool    sortBooks(SORT_METHOD sm);
-    
+	BookDB(std::string filename, std::string salesFilename = "sales_data.txt");                // 2. Require the filename and parse the text file within the code
+	~BookDB();
+public:
+	// Getters
+	Book*    getBooks();
+	Book     getBook(int index);
+	int      getNumBooks();
+
+	double   getWholesaleValue();
+	double  getRetailValue();
+
+	// Mutators
+	bool    readBooks(std::string filename);
+	bool    writeBooks(std::string filename);
+	
+	bool	readSalesData(std::string filename);
+	bool	writeSalesData(std::string filename);
+
+	double  sellBook(unsigned long ISBN);						// These cannot be combined...
+	double	sellBook(int idx);
+	bool    removeBook(unsigned long ISBN, int quantity);		// <--probably just use this one
+	bool    addBook(Book bk);
+	int     findBook(unsigned long ISBN);						// Returns the index in our array of the found entry or -1 if not found
+	void    modifyBook(unsigned long ISBN, Book replacement);	// un-needed?
+
+	bool    sortBooks(SORT_METHOD sm);
+
+
 private:
-    Book*   books;
-    int     numBooks;
-    double  wholesaleValue;
-    double  retailValue;
-    std::string DBFilename;
-    std::fstream DBFile;
+	Book*   books;
+	int     numBooks;				// number of book objects (entries in DB)
+	int     quantityItems;			// number of books including duplicate copies
+	double  totalWholesaleValue;
+	double  totalRetailValue;
+	double  currentNetProfit;		// Total profit (retail - wholesale)
+	double	currentSalesAmount;		// Basically how much $$$ should be in the register
+	std::string     DBFilename;
+	std::string		salesFilename;
+	std::fstream    DBFile;
+	std::fstream	salesFile;
 };
-
-
