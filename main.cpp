280c89306a484742c524342e9cc5048b92309020
phuzums <47841026+phuzums@users.noreply.github.com>
2019-03-03 15:30:59 -0800

    Update main.cpp
    see readme for changes

--- main.cpp
+++ main.cpp
@@ -1,83 +1,288 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 #include "BookDB.h"
 
-//NOTES::
-//  - ISBN13's look like dis: 978-1-56931-507-1... i think we should be able to just strip the dashes, right?
-//  - ISBN10's look like 164273005X ... the chars would be more work search-wise... let's just use 13's w/o dashes?
+using namespace std;
+
+void exit();
+void complete(string type, string name);
+void welcome();
+void loopReset(bool loop[]);
+void printDebug(BookDB *myDB);
+
+#define SIZE 100
+#define DEBUG false
+
 int main(int argc, char** argv)
 {
-    
-    int isbn;
-    // Test stuff out before we have file I/O working...
-    // so... make the structs by hand first.
-    Date today;
-    today.day   = 21;
-    today.month = 2;
-    today.year  = 2019;
-    
-    Book PokemonAdventures;
-    PokemonAdventures.ISBN          = 9781569315071;
-    PokemonAdventures.wholesaleCost = 474.00;
-    PokemonAdventures.retailCost    = 512.99;
-    PokemonAdventures.title         = "Pokemon Adventures";
-    PokemonAdventures.author        = "Hidenori Kusaka";
-    PokemonAdventures.publisher     = "Shogakukan";
-    PokemonAdventures.addedOn       = today;
-    
-    Book HinaMatsuri;
-    HinaMatsuri.ISBN                = 9781642730050;
-    HinaMatsuri.wholesaleCost       = 9.01;
-    HinaMatsuri.retailCost          = 9.53;
-    HinaMatsuri.title               = "Hinamatsuri Volume 1";
-    HinaMatsuri.author              = "Masao Ohtake";
-    HinaMatsuri.publisher           = "One Peace Books";
-    HinaMatsuri.addedOn             = today;
-    
-    Book LewdAnimeh;
-    LewdAnimeh.ISBN                 = 9784063840308;
-    LewdAnimeh.wholesaleCost        = 7.89;
-    LewdAnimeh.retailCost           = 99999999999.99; //#worthit
-    LewdAnimeh.title                = "Seitokai Yakuindomo 1";
-    LewdAnimeh.author               = "Tozen Ujiie";
-    LewdAnimeh.publisher            = "Kodansha";
-    LewdAnimeh.addedOn              = today;
-    
-    BookDB myDB("not_implemented_yet.nyan");
-    myDB.addBook(PokemonAdventures);
-    myDB.addBook(HinaMatsuri);
-    myDB.addBook(LewdAnimeh);
-    
-    // cout test stuff
-    
-    myDB.sortBy(SORT_METHOD::COST);
-    
-    // cout test stuff
-    
-    myDB.sellBook(9784063840308);
-    
-    // cout test stuff
-    
-    for(int idx = 0; idx < 2000; idx++)
-        myDB.addBook(HinaMatsuri);
-    
-    // more test stuff
-    
-    myDB.sortBy(SORT_METHOD::QUANTITY);
-    
-    // meow test stuff...
-    
-    
-    
-    
-    
-    
-    cout << "which book to remove?";
-    cin >> isbn;
-    
-    if (myDB.removeBook(isbn))
-        cout << "book was removed";
-    else
-        cout << "err. book not on hand...";
-    
-    return 0;
-    
+	BookDB *myDB = new BookDB("books.txt");
+	int choice[SIZE];
+	string schoice[SIZE];
+	bool loop[SIZE];
+	string type;
+	do
+	{
+
+		if (DEBUG)
+		{
+			cout << "\nChecking DB...\n";
+			printDebug(myDB);
+			cin.sync();
+		}
+
+		loopReset(loop);
+		welcome();
+		cout << "=====================================" << endl;
+		cout << "De Anza Book Shop. " << endl;
+		cout << "=====================================" << endl;
+		cout << "Modules: " << endl;
+		cout << "	1. Cashier" << endl;
+		cout << "	2. Inventory" << endl;
+		cout << "	3. Report" << endl;
+		cout << "	4. Exit" << endl;
+		cout << "=====================================" << endl;
+		cin >> choice[0];
+
+
+		if (choice[0] == 1) // CASHIER MODULE
+		{
+			system("CLS");
+			do
+			{
+				loopReset(loop);
+				cout << "=====================================" << endl;
+				cout << "Cashier Module " << endl;
+				cout << "=====================================" << endl;
+				cout << "1. Enter a book's name that you want to buy" << endl;
+				cout << "2. Enter a book's ISBN that you want to buy" << endl;
+				cout << "3. Go back" << endl;
+				cout << "=====================================" << endl;
+				cin >> choice[1];
+				if (choice[1] == 1)
+				{
+					cin.ignore();
+					system("CLS");
+					do
+					{
+						loopReset(loop);
+						cout << "=====================================" << endl;
+						cout << "Cashier Module            " << endl;
+						cout << "=====================================" << endl;
+						cout << "Please enter the name of your book" << endl;
+						cout << "1. Go back" << endl;
+						cout << "=====================================" << endl;
+						getline(cin, schoice[0]); // reads book name
+						if (schoice[0] == "1")
+						{
+							loop[2] = 0;
+							system("CLS");
+						}
+						else // schoice[0] = book...
+						{
+							type = "book";
+							complete(type, schoice[0]); // for testing purposes
+							// TODO: Implement Search-by-title here
+						}
+					} while (loop[2]);
+
+				}
+				else if (choice[1] == 2)
+				{
+					cin.ignore();
+					system("CLS");
+					do
+					{
+						loopReset(loop);
+						cout << "=====================================" << endl;
+						cout << "Cashier Module            " << endl;
+						cout << "=====================================" << endl;
+						cout << "Please enter the ISBN of your book" << endl;
+						cout << "1. Go back" << endl;
+						cout << "=====================================" << endl;
+						getline(cin, schoice[1]); // reads ISBN name
+						std::cin.sync();
+						if (schoice[1] == "1")
+						{
+							loop[3] = 0;
+							system("CLS");
+						}
+						else // schoice[1] = book...
+						{
+							type = "ISBN";
+							//complete(type, schoice[1]); // for testing purposes
+							int idx = myDB->findBook(stol(schoice[1]));
+							if (idx < 0)
+								cout << "\nBook could not be found... Please try again.\n";
+							else
+								if (myDB->getBook(idx).quantity <= 0)
+									cout << "\nBook is currently sold out... Please try another book.\n";
+								else
+								{
+									double price = myDB->sellBook(idx);
+									cout << "\n" << '\"' << myDB->getBook(idx).title << '\"' << " was sold for: " << setprecision(2) << fixed << price << endl;
+								}
+							cout << "\nPress any key...";
+							system("pause >nul");
+							system("cls");
+						}
+					} while (loop[3]);
+				}
+				else if (choice[1] == 3) // go back
+				{
+					loop[1] = 0;
+					system("CLS");
+				}
+				else
+				{
+					system("CLS");
+					cout << endl << "Please try again by entering a number 1-3. " << endl << endl;
+				}
+			} while (loop[1]);
+		}
+		else if (choice[0] == 2) // INVENTORY MODULE
+		{
+			system("CLS");
+			do
+			{
+				loopReset(loop);
+				cout << "=====================================" << endl;
+				cout << "Welcome to the Inventory Module " << endl;
+				cout << "=====================================" << endl;
+				cout << "1. Search for a book" << endl;
+				cout << "2. Add a book" << endl;
+				cout << "3. Delete a book" << endl;
+				cout << "4. Edit a book" << endl;
+				cout << "5. Go back" << endl;
+				cout << "=====================================" << endl;
+				cin >> choice[2];
+				if (choice[2] == 1)
+				{
+
+				}
+				else if (choice[2] == 2)
+				{
+
+				}
+				else if (choice[2] == 3)
+				{
+					loop[4] = 0;
+				}
+				else
+				{
+					cout << endl << "You have typed in an invalid bookname/ISBN, please try again. " << endl << endl;
+				}
+			} while (loop[4]);
+
+		}
+		else if (choice[0] == 3)
+		{
+
+		}
+		else if (choice[0] == 4)
+		{
+		delete myDB;
+		cout << "\nPress any key...";
+		system("pause >nul");
+			exit();
+		}
+		else
+		{
+			system("CLS");
+			cout << endl << "Please try again by entering a number 1-4. " << endl << endl;
+		}
+
+	} while (loop[0]);
+
+
+
+
+
+	exit();
+
+
+
+	return 0;
+
+}
+
+void exit()
+{
+	system("CLS");
+	cout << " _ _ _ " << endl;
+	cout << "| | | | | | " << endl;
+	cout << "| || |__ __ _ _ __ | | ___ _ ___ _ _ " << endl;
+	cout << "| _| ' \ / ` | ' \| |/ / | | |/ _ \| | | | " << endl;
+	cout << "| || | | | (| | | | | <| || | () | || | " << endl;
+	cout << " \|| ||\,|| |||\\\, |\/ \__,| " << endl;
+	cout << " / | " << endl;
+	cout << " |/ " << endl << endl;
+	cout << "For using our app. Safe Travels. U w U. ";
+
+	cout << endl << "Press enter to exit the program. " << endl;
+	cin.get();
+	cin.ignore();
+	exit(0);
+}
+
+void complete(string type, string name)
+{
+	system("CLS");
+	cout << "Your " << type << ", " << """ <<name << """ << " has been added to the store. " << endl << endl;
+}
+
+void welcome()
+{
+	cout << " _ _ _ _ _ _ " << endl;
+	cout << "| | | | | | | | | | | | " << endl;
+	cout << "| | | | | | ___ ___ _ __ ___ ___ | | ___ | || |_ ___ " << endl;
+	cout << "| |/\| |/ _ \ |/ / _ \| ' _ \\ / _ \\ | __/ _ \\ | __| '_ \\ / _ \\ " << endl; cout << "\\ /\\ / __/ | (_| (_) | | | | | | __/ | || (_) | | |_| | | | __/ " << endl; cout << " \\/ \\/ \\___|_|\\___\\___/|_| |_| |_|\\___| \\__\\___/ \\__|_| |_|\\___| " << endl; cout << "	______ ___ " << endl; cout << "	| _ \\ / _ \\ " << endl; cout << "	| | | |___/ /_\\ \\_ __ ______ _" << endl; cout << "	| | | / _ \\ _ | '_ \\|_ / _ | " << endl;
+	cout << "	| |/ / / | | | | | |/ / (| | " << endl;
+	cout << "	|/ \\| |/| |/\,| " << endl;
+	cout << "	______ _ _____ _ " << endl;
+	cout << "	| ___ \ | | / | | " << endl;
+	cout << "	| |/ / ___ ___ | | __ \ --.| |__ ___ _ __ " << endl; cout << "	| ___ \\/ _ \\ / _ \\| |/ / --. \ ' \ / _ \| ' \ " << endl;
+	cout << "	| |/ / () | () | < /\__/ / | | | () | |) | " << endl;
+	cout << "	\/ \/ \/||\\ \/|| ||\/| ./ " << endl;
+	cout << "	| | " << endl;
+	cout << "	|_| " << endl;
+	cout << endl << "Press enter to start the program. " << endl;
+	cin.get();
+	system("CLS");
+}
+
+void loopReset(bool loop[])
+{
+	for (int i = 0; i <= SIZE; i++) // sets all values of loop into true
+	{
+		loop[i] = 1;
+	}
+}
+
+
+void printDebug(BookDB *myDB)
+{
+	std::cout << "\nPrint all books? (y/n) : ";
+	char meow;
+	std::cin >> meow;
+	if(meow == 'y')
+		for (int idx = 0; idx < myDB->getNumBooks(); idx++)
+			std::cout << "\nBook " << idx << ':' << myDB->getBook(idx);
+
+	std::cout << "\nTotal Wholesale Worth: " << myDB->getWholesaleValue();
+	std::cout << "\nTotal Retail Worth: " << myDB->getRetailValue();
+	std::cout << "\nBooks in DB: " << myDB->getNumBooks();
+	std::cout << "\n====================================";
+
+	if (meow == 'y')
+	{
+		std::cout << "\n\n  ~ Original Book List ~";
+		for (int idx = 0; idx < myDB->getNumBooks(); idx++)
+			std::cout << "\nBook " << idx << ':' << myDB->getBook(idx).title;
+		std::cout << "\n====================================";
+	}
+
+	std::cout << "\n\n";
+	system("pause >nul");
+	std::cin.sync();
 }
