71a6247fc231d415839d3f157a7af090d79e2c64
phuzums <47841026+phuzums@users.noreply.github.com>
2019-03-14 13:45:47 -0700

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -1,1 +1,22 @@
 #include "Book.h"
+
+std::ostream& operator <<(std::ostream &out, const Book &bk)
+{
+	std::cout << "\n  Title: " << bk.title << "\n  Author: " << bk.author << "\n  Cost: " << bk.retailCost
+		<< "\n  Wholesale: " << bk.wholesaleCost << "\n  Publisher: " << bk.publisher
+		<< "\n  ISBN: " << bk.ISBN << "\n  Quantity: " << bk.quantity;
+	return out;
+}
+
+
+void Book::operator=(const Book &right) {
+	this->title = right.title;
+	this->ISBN = right.ISBN;
+	this->author = right.author;
+	this->wholesaleCost = right.wholesaleCost;
+	this->retailCost = right.retailCost;
+	this->publisher = right.publisher;
+	this->addedOn.day = right.addedOn.day;
+	this->addedOn.month = right.addedOn.month;
+	this->addedOn.year = right.addedOn.year;
+};
