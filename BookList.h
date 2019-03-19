46f275dae4eee6495b939860b67228218a2fb830
phuzums <47841026+phuzums@users.noreply.github.com>
2019-03-18 22:33:47 -0700

    Update BookList.h

--- BookList.h
+++ BookList.h
@@ -9,7 +9,7 @@ class BookList
 {
 public:
 	BookList();
-	~BookList();
+	virtual ~BookList();
 public:
 	Book*			getBooks();
 	Book			getBook(int index);
@@ -20,13 +20,44 @@ public:
 	bool			addBook(const Book &bk);
 
 	int				findBook(unsigned long ISBN);						// Returns the index in our array of the found entry or -1 if not found
-	int				findBook(std::string title);
+	int				findBook(std::string title);		// Note there can only be one book per title (other editions etc. have varying titles)
+	BookList*		findBookByAuthor(std::string author);
+	BookList*		findBookByPublisher(std::string publisher);
+	BookList*		findBookByDate(Date myDate);
+	BookList*		findBookByQuantity(int amt); // Not sure when anyone would ever need this (maybe to find low stocks) but hey...
+	
+	template<typename T>
+	BookList*		findBookByRetailPrice(T price);
+
+	template<typename T>
+	BookList*		findBookByWholesalePrice(T price);
 
 	double			getTotalCost();
-	void			modifyBook(unsigned long ISBN, Book replacement);	// un-needed?
+	void			modifyBook(int idx, Book replacement);
+	virtual void	printByMethod(int verbosity, SORT_METHOD sm);
 
 protected:
 	Book*			books;
 	int				numBooks;				// number of book objects (entries in DB)
 
 };
+
+template<typename T>
+BookList* BookList::findBookByRetailPrice(T price)
+{
+	BookList *t_list = new BookList();
+	for (int idx = 0; idx < numBooks; idx++)
+		if (books[idx].retailCost == price)
+			t_list->addBook(books[idx]);
+	return t_list;
+}
+
+template<typename T>
+BookList* BookList::findBookByWholesalePrice(T price)
+{
+	BookList *t_list = new BookList();
+	for (int idx = 0; idx < numBooks; idx++)
+		if (books[idx].wholesaleCost == price)
+			t_list->addBook(books[idx]);
+	return t_list;
+}
